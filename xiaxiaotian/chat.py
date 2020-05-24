fd8aa7f41e88e4bd60f5b644efa44d6033ab8b38
XiaSweet <lolosweet@storymail.xiasweet.com>
2020-05-24 19:45:19 +0800

    优化详细日志到机器人界面

--- xiaxiaotian/chat.py
+++ xiaxiaotian/chat.py
@@ -6,6 +6,9 @@ import time
 import json
 import ssl
 import subprocess
+#日志记录模块
+import logging
+from nonebot.log import logger
 #注意：用户的想说的话在这里的变量是'chat'
 
 
@@ -18,7 +21,7 @@ async def aichat(session: CommandSession):
 
     # 通过封装的函数获取图灵机器人的回复
     aichat_re = await call_txai_api(chat)
-
+       
     await session.send(aichat_re,at_sender=True)
 
 # aichat.args_parser 装饰器将函数声明为 aichat 命令的参数解析器
@@ -31,11 +34,28 @@ async def _(session: CommandSession):
  
 #开始运行命令
 async def call_txai_api(chat: str) -> str:
-
+    logger.info('[腾讯闲聊]开始像脚本发送变量并尝试获取回复')
     # subprocess.getoutput是用于执行脚本并输出结果的
     hfchat = subprocess.getoutput("python3 lib/txai_chat/chat.py -c %s"%(chat))
-    # 向用户返回的信息（这里是脚本内容）
-    return f'{hfchat}'
+    if hfchat == 'ERROR4':
+        logger.info('[腾讯闲聊]出现错误了，代码:4 - TXAI16394')
+        logger.info('[腾讯闲聊]指令出现错误并结束')
+        return f'你的问题好像有些为难我哦。请换个话题聊聊吧◑﹏◐'
+    elif hfchat == 'ERROR6':
+        logger.info('[腾讯闲聊]出现错误了，代码:6 - TXAI-YourNetworkBad')
+        logger.info('[腾讯闲聊]指令出现错误并结束')
+        return f'通往次元的轮船堵船啦，请稍后再找我吧-_-|||'
+    elif hfchat == 'ERROR5':
+        logger.info('[腾讯闲聊]出现错误了，代码:5 - TXAI16394')
+        logger.info('[腾讯闲聊]小提醒：您无法独自解决本问题，请联系管理员协助您处理吧')
+        logger.info('[腾讯闲聊]指令出现错误并结束')
+        
+        return f'因为小管家意外受伤了，请呼唤管理员来拯救吧'
+    else: 
+        # 向用户返回的信息（这里是脚本内容）
+        logger.info('[腾讯闲聊]没有出现错误，正常向用户发送结果')
+        logger.info('[腾讯闲聊]指令成功完成并结束')
+        return f'{hfchat}'
 
     
 # on_natural_language 装饰器将函数声明为一个自然语言处理器
