2569b18f104f62b1c7485b1a30cd75d8dfee0048
XiaSweet <lolosweet@storymail.xiasweet.com>
2020-05-27 21:47:51 +0800

    添加SDK，测试统一变量

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 from nonebot.default_config import *
 from datetime import timedelta
-
+#欢迎进入夏小甜De配置文件夹
+#以下信息为设置小管家的专属文件，如果不清楚设置的内容则不建议精简本行注释代码
 SUPERUSERS = {1172608638}
 COMMAND_START = {'', '/', '!', '／', '！'}
 NICKNAME = {'小管家', '夏小甜'}
@@ -14,3 +15,16 @@ SESSION_CANCEL_EXPRESSION = (
 SESSION_RUN_TIMEOUT = timedelta(seconds=10)
 DEFAULT_VALIDATION_FAILURE_EXPRESSION = '你发送的内容格式不太对呢，请检查一下再发送哦～'
 DEBUG = False
+#腾讯闲聊不知道如何回答的情况回答库，将会在以下内容内随机挑选一个
+TXCHAT_BAD = (
+    '你的问题好像有些为难我哦。请换个话题聊聊吧◑﹏◐',
+    '问这么深奥的问题你是故意的吧？',
+    '我还小，你不能问这么深奥的问题在欺负我╭(╯^╰)╮',
+    '啦啦啦，我是无情的小管家。我听不见你说话啦啦啦(ˉ﹃ˉ)',
+    '我还不太明白你在说什么呢，没关系，以后的我会变得更强呢！',
+    '？？？，我想跟你聊些简单的话题',
+    '其实我不太明白你的意思……',
+    '抱歉哦，我现在的能力还不能明白你在说什么，但我会加油的～',
+    '本管家现在不想理你，不接受质疑(︶︿︶)╭∩╮',
+)
+#配置文件结束
--- xiaxiaotian/chat.py
+++ xiaxiaotian/chat.py
@@ -1,67 +1,42 @@
 from nonebot import on_command, CommandSession
 from nonebot import on_natural_language, NLPSession, IntentCommand
-#加载AI组件
+from nonebot.helpers import context_id, render_expression as expr
+from nonebot import get_bot
 import optparse
 import time
 import json
 import ssl
 import subprocess
-#日志记录模块
 import logging
 from nonebot.log import logger
-#注意：用户的想说的话在这里的变量是'chat'
-
-
-# on_command 装饰器将函数声明为一个命令处理器
-# 这里 aichat 为命令的名字（可为不冲突的任意值），同时允许使用别名「跟我聊天」   
+TXCHAT_BAD = get_bot().config.TXCHAT_BAD
 @on_command('aichat',only_to_me=True)
 async def aichat(session: CommandSession):
-    # 获取可选参数，这里如果没有 message 参数，命令不会被中断，message 变量会是 None
     chat = session.state.get('chat')
     logger.info('[腾讯闲聊]指令被触发了，开始运行')
-    # 通过封装的函数获取图灵机器人的回复
     aichat_re = await call_txai_api(chat)
-       
-    await session.send(aichat_re,at_sender=True)
-
-# aichat.args_parser 装饰器将函数声明为 aichat 命令的参数解析器
-# 命令解析器用于将用户输入的参数解析成命令真正需要的数据
-@aichat.args_parser
-async def _(session: CommandSession):
-    # 去掉消息首尾的空白符和混淆注释号’#’
-    stripped_arg = session.current_arg_text.strip(' ')
-    stripped_arg = session.current_arg_text.strip('#')
- 
-#开始运行命令
-async def call_txai_api(chat: str) -> str:
-    logger.info('[腾讯闲聊]开始像脚本发送变量并尝试获取回复')
-    # subprocess.getoutput是用于执行脚本并输出结果的
-    hfchat = subprocess.getoutput("python3 lib/txai_chat/chat.py -c '%s'"%(chat))
-    if hfchat == 'ERROR4':
-        logger.info('[腾讯闲聊]出现错误了，代码:4 - TXAI16394')
-        logger.info('[腾讯闲聊]指令出现错误并结束')
-        return f'你的问题好像有些为难我哦。请换个话题聊聊吧◑﹏◐'
-    elif hfchat == 'ERROR6':
+    if aichat_re == 'ERROR4':
+        logger.info('[腾讯闲聊]出现意外了，腾讯端还不会回答用户的问题，代码:4 - TXAI16394')
+        logger.info('[腾讯闲聊]向用户告知不会回答并结束')
+        await session.send(expr(TXCHAT_BAD))
+    elif aichat_re == 'ERROR6':
         logger.info('[腾讯闲聊]出现错误了，代码:6 - TXAI-YourNetworkBad')
+        logger.debug('[腾讯闲聊]小提醒：网络堵塞，请重新触发即可')
         logger.info('[腾讯闲聊]指令出现错误并结束')
-        return f'通往次元的轮船堵船啦，请稍后再找我吧-_-|||'
-    elif hfchat == 'ERROR5':
+        await session.send('通往次元的轮船堵船啦，请稍后再找我吧-_-|||')
+    elif aichat_re == 'ERROR5':
         logger.info('[腾讯闲聊]出现错误了，代码:5 - TXAI16394')
         logger.info('[腾讯闲聊]小提醒：您无法独自解决本问题，请联系管理员协助您处理吧')
         logger.info('[腾讯闲聊]指令出现错误并结束')    
-        return f'因为小管家意外受伤了，请呼唤管理员来拯救吧'
-    else: 
-        # 向用户返回的信息（这里是脚本内容）
+        await session.send('因为小管家意外受伤了，请呼唤管理员来拯救吧')
+    else:
         logger.info('[腾讯闲聊]没有出现错误，正常向用户发送结果')
         logger.info('[腾讯闲聊]指令成功完成并结束')
-        return f'{hfchat}'
-
-    
-# on_natural_language 装饰器将函数声明为一个自然语言处理器
-# keywords 表示需要响应的关键词，类型为任意可迭代对象，元素类型为 str
-# 如果不传入 keywords，则响应所有没有被当作命令处理的消息
+        await session.send(aichat_re,at_sender=True)
+async def call_txai_api(chat: str) -> str:
+    logger.info('[腾讯闲聊]开始向脚本发送变量并尝试获取回复')
+    hfchat = subprocess.getoutput("python3 lib/txai_chat/chat.py -c '%s'"%(chat))
+    return f'{hfchat}'
 @on_natural_language(keywords=None,only_to_me=True)
 async def _(session: NLPSession):
-    # 返回意图命令，前两个参数必填，分别表示置信度和意图命令名
-    # 确保任何消息都在且仅在其它自然语言处理器无法理解的时候使用 aichat 命令
     return IntentCommand(60.0, 'aichat', args={'chat': session.msg_text.replace(' ','+')})
